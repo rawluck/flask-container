c8cc45f208607080a7e5db34a17966363833c96d
Lakshya Rawat <lakshyarawat20@gmail.com>
2021-07-09 11:53:22 -0400

    Take port from environment variable

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Containerized Hello, World! with CI/CD using Travis CI by Lakshya Rawat, Github Repo: https://github.com/grofers/flask-hello-world '
+    return 'Containerized Hello, World! with CI/CD using Travis CI by Lakshya Rawat, <a href=https://github.com/rawluck/flask-container>Github Repo</a> '
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
