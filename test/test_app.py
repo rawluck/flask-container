8f311448a0ee8ef0b94389afba6de378e20b3cff
Lakshya Rawat <lakshyarawat20@gmail.com>
2021-07-09 11:57:37 -0400

    Update test

--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,4 @@
 import unittest 
 import app
 def test_test():
-    assert app.hello_world() == 'Containerized Hello, World! with CI/CD using Travis CI by Lakshya Rawat, <a href=https://github.com/[secure]/flask-container>Github Repo</a>'
+    assert app.hello_world() == 'Containerized Hello, World! with CI/CD using Travis CI by Lakshya Rawat, <a href=https://github.com/rawluck/flask-container>Github Repo</a>'
