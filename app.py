5c91a6cc5668e0495c8dbdb6a8a4cc296e9c5ecb
Lakshya Rawat <lakshyarawat20@gmail.com>
2021-07-09 11:47:05 -0400

    Added dynamic port from environmet variable

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Containerized Hello, World! with CI/CD using Travis CI by Lakshya Rawat, Github Repo: https://github.com/grofers/flask-hello-world '
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
